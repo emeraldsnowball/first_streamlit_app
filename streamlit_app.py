2acf602f8fa72c542a7e58dd14a14a8350be34f5
emeraldsnowball <68956989+emeraldsnowball@users.noreply.github.com>
2023-07-04 15:58:47 +0530

    Update streamlit_app.py
    table error fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,10 +18,10 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 #display the table on the page
-streamlit.dataframe(fruits_to_show.json())
+streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
