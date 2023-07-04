990b658a028134f27905b99d02d40278a0050d08
emeraldsnowball <68956989+emeraldsnowball@users.noreply.github.com>
2023-07-04 15:58:02 +0530

    Update streamlit_app.py
    fix for json format

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 #display the table on the page
-streamlit.dataframe(fruits_to_show)
+streamlit.dataframe(fruits_to_show.json())
 
 streamlit.header("Fruityvice Fruit Advice!")
 
